2b0e740525b9152791a0fa26762a2ed1e46b12f6
devesh22s <devesh262004@gmail.com>
2026-03-06 23:28:39 +0530

    User Authentication added

---
+++ server/controllers/userController.js
@@ -0,0 +1,58 @@
+import userModel from "../Model/userModel";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+export const registerUser = async(req, res)=>{
+    try{
+        const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.json({ success: false, message: 'Missing Details' });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt)
+  const userData ={
+    name,
+    email,
+    password: hashPassword
+  }
+  const newUser = new userModel(userData)
+  const user = await newUser.save();
+  const token = jwt.sign({id:user._id}, process.env.JWT_Secret)
+  res.json({success: true, token, user:{name : user.name}})
+
+
+
+    }catch(error){
+      console.log(error);
+      res.json({success :false, message:error.message})
+      
+
+    }
+}
+
+
+export const loginUser = async(req, res)=>{
+  try {
+    const {email, password} = req.body;
+    const user = await userModel.findOne({email});
+    if(!user){
+      return res({success: false, message: "User doesn't exist"})
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(isMatch){
+      const token = jwt.sign({id:user._id}, process.env.JWT_Secret)
+      res.json({success: true, token, user:{name: user.name}})
+    }else{
+      return res({success: false, message: 'Invalid credentials'})
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({success: false, message:error.message})
+    
+    
+  }
+
+}
+
